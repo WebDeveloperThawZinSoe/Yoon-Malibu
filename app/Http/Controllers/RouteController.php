3ec1065c2f09e71cfb13d390e9f0fe147727347c
Thaw Zin Soe <thawzinsoe.dev@gmail.com>
2023-06-12 08:56:29 +0630

    Project Setup

---
+++ app/Http/Controllers/RouteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RouteController extends Controller
+{
+    //index
+    public function index(){
+        
+    }
+}
