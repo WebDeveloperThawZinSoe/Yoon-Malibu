dcdc586ab4abf7010aecc8e2c3b34c80270a4d1e
Thaw Zin Soe <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2023-06-16 16:13:00 +0630

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,63 @@
 @extends('layouts.admin_app')
 @section('title',"Dashboard")
 @section("content")
+<div class="container">
+    <br> <br>
+    
+    <div class="row justify-content-md-center">
+    <h3 >Dashboard</h3>
+    <br> <br>
+    
+                            <div class="col-xl-6 col-md-12">
+                                <div class="card bg-default mb-4">
+           
+                                    <div class="card-body">Category ( Yoon Kitchen ) 
+                                        <br>
+                                        <p>
+                                            Total Count : 
+                                       
+                                            <span style="display:inline-block !important;font-size:24px;padding-left:20px">
+                                                <?php 
+                                                echo DB::table("categories")->count();
+                                                ?>
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small  stretched-link" href="/category/create">
+                                            View Details
+                                        </a>
+                                        <div class="small "><svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path></svg><!-- <i class="fas fa-angle-right"></i> Font Awesome fontawesome.com --></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xl-6 col-md-12">
+                                <div class="card bg-default mb-4">
+           
+                                    <div class="card-body">Category ( Malibu Kitchen ) 
+                                        <br>
+                                        <p>
+                                            Total Count : 
+                                       
+                                            <span style="display:inline-block !important;font-size:24px;padding-left:20px">
+                                                <?php 
+                                                echo DB::table("m_categories")->count();
+                                                ?>
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small  stretched-link" href="/mcategory/create">
+                                            View Details
+                                        </a>
+                                        <div class="small "><svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path></svg><!-- <i class="fas fa-angle-right"></i> Font Awesome fontawesome.com --></div>
+                                    </div>
+                                </div>
+                            </div>
+        
+    </div>
 
 
+</div>
 @endsection
