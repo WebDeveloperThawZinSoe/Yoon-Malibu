06387207af4831ce7c4fb934ac01be26017e7a76
Thaw Zin Soe <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2023-06-18 10:22:12 +0630

    Update Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -55,6 +55,54 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <div class="col-xl-6 col-md-12">
+                                <div class="card bg-default mb-4">
+           
+                                    <div class="card-body">Menu Item ( Yoon Kitchen ) 
+                                        <br>
+                                        <p>
+                                            Total Count : 
+                                       
+                                            <span style="display:inline-block !important;font-size:24px;padding-left:20px">
+                                                <?php 
+                                                echo DB::table("menus")->count();
+                                                ?>
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small  stretched-link" href="/menu/view">
+                                            View Details
+                                        </a>
+                                        <div class="small "><svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path></svg><!-- <i class="fas fa-angle-right"></i> Font Awesome fontawesome.com --></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xl-6 col-md-12">
+                                <div class="card bg-default mb-4">
+           
+                                    <div class="card-body">Menu Item ( Malibu Kitchen ) 
+                                        <br>
+                                        <p>
+                                            Total Count : 
+                                       
+                                            <span style="display:inline-block !important;font-size:24px;padding-left:20px">
+                                                <?php 
+                                                echo DB::table("m_menus")->count();
+                                                ?>
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small  stretched-link" href="/mmenu/view">
+                                            View Details
+                                        </a>
+                                        <div class="small "><svg class="svg-inline--fa fa-angle-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path></svg><!-- <i class="fas fa-angle-right"></i> Font Awesome fontawesome.com --></div>
+                                    </div>
+                                </div>
+                            </div>
         
     </div>
 
